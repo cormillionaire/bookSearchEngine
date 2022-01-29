bc45a9d25bb2bc2a8303d8fa2b66506ccdb6495d
Cornelia Herman <corneliabherman@gmail.com>
2022-01-29 09:59:05 -0500

    worked more on the resolvers

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -2,10 +2,15 @@ const { User, Book } = require('../models');
 
 const resolvers = {
   Query: {
-    user: async () => {
-      return Matchup.findOne();
+    user: async (parent, { username, id,}) => {
+      if (args.user) {
+      return User.findOne({ username: username });
+      } else if (args.id) {
+        return User.findOne({_id : id });
+      }
+      throw new ValidationError('Cannot find a user with this id!');
     },
-    matchup: async (parent, { username }) => {
+    book: async (parent, { username }) => {
       return await User.findOne({ _id: params.id });
     },
     techs: async () => {
@@ -14,17 +19,50 @@ const resolvers = {
   },
 
   Mutation: {
-    addUser: async (parent, { id, techNum }) => {
-      return await Matchup.create(
+    addUser: async (parent, { username, email, password }) => {
+      const user =  await User.create({ username, email, password });
+      const token = signToken(user);
+      if (!user) {
+        throw new ValidationError('Something is wrong!');
+      }
+      return { token, user };
+    },
+    login: async (parent, { username, email, password }) => {
+      const  userUsername = await User.findOne({ email });
+      const userEmail = await User.findOne({ username });
+      const user = "";
+      if (!userEmail) {
+        throw new AuthenticationError('No user found with this email address');
+      } else if (!userUsername) {
+        throw new AuthenticationError('No user found with this username');
+      }
+      if (userEmail) {
+        return user = userEmail
+      }
+      const user = userUsername
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, { id, tech2 }) => {
+      if (context.user) {
+      return await User.findOneAndUpdate(
         { _id: id },
-        { $inc: { [`tech${techNum}_votes`]: 1 } },
+        {
+          $addToSet: {
+            books: [{ bookId }],
+          },
+        },
         {
           new: true,
           runValidators: true,
-        });
-    },
-    saveBook: async (parent, { tech1, tech2 }) => {
-      const user = await Matchup.findOneAndUpdate({ tech1, tech2 });
+        }
+        );
+      }
+      throw new AuthenticationError('You need to be logged in!');
     },
     deleteBook: async (parent, { tech1, tech2 }) => {
       const user = await Matchup.findOneAndUpdate({ tech1, tech2 });
